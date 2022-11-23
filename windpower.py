f05acb7a296f0224b321cb127c52073fc67adc68
Freya Helstrup <freyahelstrup@me.com>
2022-11-23 10:39:41 +0100

    Save model correctly with transformers and simplify

--- windpower.py
+++ windpower.py
@@ -16,7 +16,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor, plot_tree, export_graphviz
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit, train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # Set default values
@@ -55,46 +55,41 @@ with mlflow.start_run():
 
   # TODO: Currently the only metric is MAE. You should add more. What other metrics could you use? Why?
   metrics = [
-    ("MAE", mean_absolute_error, []),
-    ("MSE", mean_squared_error, []),
-    ("R2", r2_score, []),
+    ("MAE", mean_absolute_error),
+    ("MSE", mean_squared_error),
+    ("R2", r2_score),
   ]
 
   X = df[["Speed","Direction"]]
   y = df["Total"]
 
-  number_of_splits = 5
-
   # Log your parameters. What parameters are important to log?
   #HINT: You can get access to the transformers in your pipeline using `pipeline.steps`
   mlflow.log_param('max_depth_used',pipeline.steps[1][1].max_depth)
 
+  X_train, X_test, y_train, y_test = train_test_split(
+    X, 
+    y, 
+    test_size=0.2, 
+    shuffle=False # Shuffle = False for Datetime data, to build model on comparable timeframes
+  )
+
+  model = pipeline.fit(X_train,y_train)
+  y_pred = pipeline.predict(X_test)
+
   from matplotlib import pyplot as plt 
   figure = plt.figure(figsize=(10,3))
   plt.grid()
+  plt.plot(X_test.index, y_test, color='black', label="Actual")
+  plt.plot(X_test.index, y_pred, color='red', linestyle='dashed', label="Predicted")
 
-  for train, test in TimeSeriesSplit(number_of_splits).split(X,y):
-    pipeline.fit(X.iloc[train],y.iloc[train])
-    predictions = pipeline.predict(X.iloc[test])
-    truth = y.iloc[test]
-
-    plt.plot(truth.index, truth.values, color='black', label="Actual")
-    plt.plot(truth.index, predictions, color='red', linestyle='dashed', label="Predicted")
-
-    # Calculate and save the metrics for this fold
-    for name, func, scores in metrics:
-      score = func(truth, predictions)
-      scores.append(score)
+  # Calculate and save the metrics
+  for name, func in metrics:
+    score = func(y_test, y_pred)
+    mlflow.log_metric(name, score)
 
   plt.legend()
   plt.savefig(f"predictions.png")
   mlflow.log_artifact(f"predictions.png")
 
-  # Log a summary of the metrics
-  for name, _, scores in metrics:
-    # NOTE: Here we just log the mean of the scores. 
-    # Are there other summarizations that could be interesting?
-    mean_score = sum(scores)/number_of_splits
-    mlflow.log_metric(f"mean_{name}", mean_score)
-      
-  mlflow.sklearn.log_model(regressor, "model")
+  mlflow.sklearn.log_model(model, "model", code_paths=["custom_transformers.py"],conda_env="conda.yaml")
